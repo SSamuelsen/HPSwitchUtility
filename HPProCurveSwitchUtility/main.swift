db39d8d2a1e34c5db559e86c1792bfc0bcc1d188
Stephen Samuelsen <ssamuelsen@cedarville.edu>
2018-12-22 00:49:25 -0500

    Initial Commit

---
+++ HPProCurveSwitchUtility/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  HPProCurveSwitchUtility
+//
+//  Created by Stephen Samuelsen on 12/22/18.
+//  Copyright © 2018 Stephen Samuelsen. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
