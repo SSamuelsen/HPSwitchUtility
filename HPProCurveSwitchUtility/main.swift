09e523fb589ff15689229212a9d4ad153770db52
Stephen Samuelsen <ssamuelsen@cedarville.edu>
2018-12-29 01:31:02 -0500

    Initial Commit.

--- HPProCurveSwitchUtility/main.swift
+++ HPProCurveSwitchUtility/main.swift
@@ -6,7 +6,50 @@
 //  Copyright © 2018 Stephen Samuelsen. All rights reserved.
 //
 
-import Foundation
+import Cocoa
 
-print("Hello, World!")
+///dev/cu.usbserial-AI04JACE 9600
+
+
+class HPProCurve {
+    
+    func execute(commandName: String) {
+        
+        let process = Process()
+        process.executableURL = URL(fileURLWithPath: "/bin/\(commandName)")
+        process.arguments = []
+        process.terminationHandler = { (process) in
+            print("finished")
+        }
+        do {
+            try process.run()
+        }
+        catch {}
+        
+        
+    }//end func
+    
+ 
+}//end class
+
+
+let HpUtility = HPProCurve()
+HpUtility.execute(commandName: "ls")     //just testing to see if function works
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
